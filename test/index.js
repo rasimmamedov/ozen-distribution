4a4ea7fb942a577e53602882e3cae2fd6f4d417d
Rasim Mamedov <art99mamedov@gmail.com>
2025-05-27 21:51:44 +0500

    test-push

--- test/index.js
+++ test/index.js
@@ -4,19 +4,6 @@ import path from 'path';
 
 const app = express()
 
-app.get('/', async (req, res) => {
-
-  const browser = await puppeteer.launch({
-    headless: false,
-  });
-
-  await browser.newPage();
-
-  res.send('Hello World')
-
-
-
-
-})
+console.log('Hello World')
 
 app.listen(8080)
